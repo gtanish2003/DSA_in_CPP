a16ff12d16f907caa76ba50b0378e9442caf42fb
Tanish Gupta <gtanish2003@gmail.com>
2023-07-23 16:18:49 +0530

    Reversal of a inked list-Recursion2

---
+++ LinkedList/ReverseLLusingRecursion.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void InsertAtHead(Node *&Head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = Head;
+    Head = temp;
+}
+
+void InsertAtTail(Node *&Tail, int d)
+{
+    Node *temp = new Node(d);
+    Tail->next = temp;
+    Tail = temp;
+}
+
+void InsertAtPosition(Node* &Head, Node* &Tail, int position,int d){
+
+    if (position==1){
+        InsertAtHead(Head,d);
+        return;
+
+    }
+    Node * t=Head;
+    int count=1;
+    while(count<position-1){
+        t=t->next;
+        count++;
+    }
+
+    if (t->next==NULL){
+        InsertAtTail(Tail,d);
+        return;
+
+    }
+    Node* temp=new Node(d);
+    temp->next=t->next;
+    t->next=temp;
+}
+
+void reverse(Node*&Head,Node* curr,Node* prev){
+    if (curr== NULL){
+        Head=prev;
+        return;
+    }
+    else{
+        Node* forward=curr->next;
+        reverse(Head,forward,curr);
+        curr->next=prev;
+    }
+}
+
+Node* reverseNode(Node* &Head){
+    Node* prev=NULL;
+    Node* curr=Head;
+    reverse(Head,curr,prev);
+    return Head;
+    
+   
+}
+void print(Node *&Head)
+{
+    Node *temp = Head;
+    while (temp)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{ // Creating a new node
+    Node *node1 = new Node(10);
+    cout << node1->data << endl;
+    cout << node1->next << endl;
+
+    // Pointing head to node1
+    Node *Head = node1;
+
+    // Pointing tail to node1
+    Node *Tail = node1;
+
+    // Insertion at head
+    InsertAtHead(Head, 20);
+    InsertAtHead(Head, 30);
+    InsertAtHead(Head, 40);
+    InsertAtHead(Head, 50);
+
+    // Insertion at tail
+    InsertAtTail(Tail, 200);
+    InsertAtTail(Tail, 300);
+    InsertAtTail(Tail, 400);
+    InsertAtTail(Tail, 500);
+
+    //Insertion at any position
+    InsertAtPosition(Head,Tail,3,1234);
+
+    // Displaying or traversing the linked list
+    print(Head);
+
+    Node* newNode=reverseNode(Head);
+    cout<<"Linked list after reversing "<<endl;
+    print(newNode);
+
+    return 0;
+}
