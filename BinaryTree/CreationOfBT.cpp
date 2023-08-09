8eb66260d128bb5e479d404d8cc9023da3ddfbd0
Tanish Gupta <gtanish2003@gmail.com>
2023-08-09 13:02:49 +0530

    Creation of BT completed

--- BinaryTree/CreationOfBT.cpp
+++ BinaryTree/CreationOfBT.cpp
@@ -27,10 +27,12 @@ class Node{
             return NULL;
         }
 
-        cout<<"Enter the data for inserting in left:"<<endl;
+        cout<<"Enter the data for inserting in left:"<<data<<endl;
         root->left=buildTree(root->left);
-        cout<<"Enter the data for inserting in right:"<<endl;
+        cout<<"Enter the data for inserting in right:"<<data<<endl;
         root->right=buildTree(root->right);
+
+        return root;
     }
 
 
