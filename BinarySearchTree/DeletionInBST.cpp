bd7978f80b47c59dacfb7b62c01798ec7b1328bc
Tanish Gupta <gtanish2003@gmail.com>
2023-08-22 13:45:55 +0530

    Deletion in a BST

---
+++ BinarySearchTree/DeletionInBST.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d){
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+void deletionInBST(Node*root){
+    
+}
